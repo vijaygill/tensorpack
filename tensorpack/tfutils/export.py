48f4c2c8113622d2d715004e95713744f14f24ed
Vijay Gill <vijay.gill@hotmail.com>
2020-04-15 00:24:51 +0100

    If tags is not supplied, use default value BUT that too as tuple.

--- tensorpack/tfutils/export.py
+++ tensorpack/tfutils/export.py
@@ -114,8 +114,8 @@ class ModelExporter(object):
             https://github.com/tensorflow/serving/blob/master/tensorflow_serving/g3doc/signature_defs.md
         """
         if tags is None:
-            tags = (tf.saved_model.SERVING if get_tf_version_tuple() >= (1, 12)
-                    else tf.saved_model.tag_constants.SERVING)
+            tags = [(tf.saved_model.SERVING if get_tf_version_tuple() >= (1, 12)
+                    else tf.saved_model.tag_constants.SERVING)]
 
         self.graph = self.config._maybe_create_graph()
         with self.graph.as_default():
